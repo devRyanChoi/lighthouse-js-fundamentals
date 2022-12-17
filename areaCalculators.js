8070dc1d23ae17026c9f38a287afde6e15b64664
devRyanChoi <mgc202077@gmail.com>
2022-12-17 02:03:54 -0500

    commit

--- areaCalculators.js
+++ areaCalculators.js
@@ -1,3 +1,23 @@
-const areaCalculator = calculateRectangleArea(length, width){
-  return
+function calculateRectangleArea(length, width){
+  if(length < 0 || width < 0){
+    return undefined;
+  } else {
+    return length*width;
+  }
+}
+
+function calculateTriangleArea(base, height) {
+  if(base < 0 || height < 0) {
+    return undefined;
+  } else {
+    return base * height / 2;
+  }
+}
+
+function calculateCircleArea(radius) {
+  if(radius < 0) {
+    return undefined;
+  } else {
+    return Math.pi * (radius**2);
+  }  
 }
